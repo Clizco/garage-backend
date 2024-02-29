d414521bab43553d6fdc215f58b3ba9362ee8ea6
Clizco <gonzalezad06@gmail.com>
2024-02-29 17:23:06 -0500

    Create db.sql

---
+++ database/db.sql
@@ -0,0 +1,18 @@
+-- Creando la base de datos --
+CREATE DATABASE customers;
+
+-- Accediendo a la base de datos --
+use customers;
+
+-- Creando la tabla --
+CREATE TABLE customer (
+    id INT(6) AUTO_INCREMENT PRIMARY KEY,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    user_email VARCHAR(190) UNIQUE NOT NULL,
+    user_password VARBINARY(20) NOT NULL,
+    birth_date DATE NOT NULL,
+    user_phone INT(10) UNIQUE NOT NULL,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
