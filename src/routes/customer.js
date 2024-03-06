7b0886e6abb2985f330289364e6fa4c1c2d6b6ac
Clizco <gonzalezad06@gmail.com>
2024-03-06 14:44:54 -0500

    Update customer.js

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,6 +3,7 @@ import {
     createCustomers,
     deleteCustomer,
     editCustomer,
+    loginCustomer,
     renderCustomers,
     updateCustomer,
 } from "../controllers/customerController.js";
@@ -12,13 +13,14 @@ const accountRouter = Router()
 
 // middleware that logs the IP
 accountRouter.use((req, res, next) => {
-    console.log(req.ip)
+    console.log(req.ip) 
     next();
 })
-
+ 
 
 accountRouter.get('/', renderCustomers);
 accountRouter.post('/add', createCustomers);
+accountRouter.post('/login', loginCustomer)
 accountRouter.get('/update/:id', editCustomer);
 accountRouter.post('/update/:id', updateCustomer);
 accountRouter.get('/delete/:id', deleteCustomer);
@@ -26,6 +28,10 @@ accountRouter.get('/delete/:id', deleteCustomer);
 // RUTAS PARA LOS EJS
 accountRouter.get('/signup', function(req, res){
     res.render('pages/signup', {title: 'Signup Page'})
+});
+
+accountRouter.get('/login', function(req, res){
+    res.render('pages/login', {title: 'Login Page'})
 });
 
 
