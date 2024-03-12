f285dbb7e42aab3599c9603968305f0bed46d5cf
Clizco <gonzalezad06@gmail.com>
2024-03-11 19:00:15 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
 import express, { urlencoded } from 'express';
 import path from "path";
 import { fileURLToPath } from 'url';
 import morgan from 'morgan';
 import accountRouter from './routes/customer.js';
+import cookieParser from "cookie-parser";
 
+dotenv.config()
+
+// BUG FIX FOR VIEWS
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -13,10 +18,20 @@ expressApp.set('view engine', 'ejs');
 expressApp.set("port", process.env.PORT || 3000);
 expressApp.set('views', path.join(__dirname, 'views'));
 
+// middleware that logs the IP
+accountRouter.use((req, res, next) => {
+    console.log(req.ip);
+  
+    next();
+}); 
+
 // middlewares
 
 expressApp.use(morgan('dev'));
 expressApp.use(express.urlencoded({ extended: false }));
+expressApp.use(cookieParser())
+expressApp.use(express.text())
+expressApp.use(express.json())
 
 // routes
 expressApp.use("/", accountRouter)
