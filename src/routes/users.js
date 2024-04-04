0f5d839d4e81145599fd117bf8d154c3a1a8eec0
Clizco <gonzalezad06@gmail.com>
2024-04-03 19:02:10 -0500

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,8 +4,9 @@ import { pool } from "../db.js";
 import jwt from "jsonwebtoken";
 import { jwtDecode } from "jwt-decode";
 import config from "../config.js";
-import bcrypt from "bcryptjs"
-
+import bcrypt from "bcrypt"
+import mysql from "mysql"
+ 
 const saltRounds = 10;
 
 const userRouter = Router()
@@ -60,46 +61,99 @@ userRouter.get("/users/token", async (req, res) => {
 //se encarga de crear los usuarios
 userRouter.post("/signup/", validateCreate, async (req, res) => {
 
-    const password = req.body.user_password;
-    const encryptedPassword = await bcrypt.hash(password, saltRounds)
+    bcrypt.genSalt(10, function(err, salt){
+        bcrypt.hash(req.body.user_password, salt, function(err, hash){
 
-    const newUser = ({ "first_name":req.body.first_name,
-                       "last_name":req.body.last_name, 
-                       "user_email":req.body.user_email, 
-                       "user_password":encryptedPassword, 
-                       "birth_date":req.body.birth_date, 
-                       "user_phone":req.body.user_phone 
-                    })
+        const newUser = ({ "first_name":req.body.first_name,
+                           "last_name":req.body.last_name, 
+                           "user_email":req.body.user_email, 
+                           "user_password":hash, 
+                           "birth_date":req.body.birth_date, 
+                           "user_phone":req.body.user_phone 
+                        })
+    
+      
+                        
+        const token = jwt.sign({id: newUser._id}, config.secret, {
+            expiresIn: 60 * 60 * 24
+        })
 
-  
-                    
-    const token = jwt.sign({id: newUser._id}, config.secret, {
-        expiresIn: 60 * 60 * 24
+        const result = pool.query("INSERT INTO customer set ?", [newUser]);
+        
+        
+        res.json({auth: true, result, token}); 
+
+
+        })
+
+        
+
     })
 
-
-
-    const result = await pool.query("INSERT INTO customer set ?", [newUser]);
-        
-        
-    res.json({auth: true, result, token}); 
     
 });
 
 
 //inicio de sesion de los usuarios
 userRouter.post("/signin/", async (req, res) => {
+    try {
+    
 
-    const { user_email, user_password} = req.body
+    const email = req.body.user_email;
+    const password = req.body.user_password;
 
-    const result =  await pool.query(`SELECT * FROM customer WHERE user_email = '${user_email}' AND user_password '${user_password}'`);
-    if (err) throw err;
-    if (result.length > 0) {
-        res.json(200)
-    } else {
-        res.send('Invalid login credentials');
+    const query = "SELECT * FROM customer WHERE user_email = ?";
+
+
+    const [currentUser] = await pool.query(query, [email]);
+    // console.log(currentUser)
+
+    if(!currentUser){
+        res.status(401).send("Credenciales incorrectas");
+        return;
+    }
+    
+    const user = currentUser[0]
+    // console.log(user)
+    const result = await bcrypt.compareSync(password, user.user_password);
+
+    console.log(result)
+    if(!result) {
+        res.status(401)
+        res.send("Credenciales incorrectas");
+        return;
+    }
+ 
+    res.status(201)
+    res.send("Inicio de sesion exitoso");
+    console.log(user)
+    } catch(error){
+        console.error(error)
+        res.status(500)
+        res.send("Error interno del servidor")
+
     }
 });
+
+
+
+//actualizacion de los datos de un usuario
+userRouter.post("/update/:id", async(req, res) => {
+    try {
+    const { id } = req.params;
+    
+    const newCustomer = req.body;
+
+    const result = await pool.query("UPDATE customer set ? WHERE id = ?", [newCustomer, id])
+
+    res.json(result)
+
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+})
+
 
 //delete users
 userRouter.delete("/delete/:email", async(req, res) => {
