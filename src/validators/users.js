7e0ced6636122ccd5c7bceac12b12b3edce11bd4
Clizco <gonzalezad06@gmail.com>
2024-04-10 17:04:31 -0500

    Update users.js

--- src/validators/users.js
+++ src/validators/users.js
@@ -1,48 +1,25 @@
-import { check } from "express-validator";
+import { body, check } from "express-validator";
 import { validateResult } from "../helpers/validateHelpers.js";
 
 const validateCreate = [
-    // USER FIRSTNAME VALIDATION
-    check('first_name')
-    .custom((value) => {
-        return value.match(/^[A-Za-z ]+$/);
-      })
-      .withMessage('Do not use special characters within the name'),
+   
+    // first_name validation
+    check('first_name').not().isEmpty().custom((value) => { return value.match(/^[A-Za-z ]+$/); }).withMessage('Do not use special characters within the name'), (req, res, next) => { validateResult(req, res, next); },
     
-    // USER LASTNAME VALIDATION
-    check('last_name')
-    .custom((value) => {
-        return value.match(/^[A-Za-z ]+$/);
-    })
-        .withMessage('Do not use special characters within the name'),
+    // last_name validation
+    check('last_name').not().isEmpty().custom((value) => { return value.match(/^[A-Za-z ]+$/); }).withMessage('Do not use special characters within the name'), (req, res, next) => { validateResult(req, res, next); },
 
+    // user_email validation
+    check('user_email').isEmail().withMessage('Please enter valid email'), (req, res, next) => { validateResult(req, res, next); } ,
 
-
-    // USER EMAIL VALIDATION
-    check('user_email')
-    .isEmail()
-    .withMessage('Please enter valid email'),
-            (req, res, next) => {
-        validateResult(req, res, next);
-
-    },
-
+    // user_password validation
     check('user_password'),
 
-    check('birth_date'),
+    // birth_date validation
+    check('birth_date').not().isEmpty().withMessage('please fill this camp').toDate().optional({ checkFalsy: true}).withMessage('Please enter valid dateofbirth'), (req, res, next) => { validateResult(req, res, next); },
 
+    check('user_phone').not().isEmpty().custom((value) => { return value.match(/^[0-9 ]*$/); }).withMessage('Do not use special characters within the name'), (req, res, next) => { validateResult(req, res, next); },
 
-    check('user_phone')
-        .custom((value) => {
-        return value.match(/^[1-1-10 ]+$/);
-
-      }).withMessage('Do not use special characters within the name')
-        .isNumeric()
-        .withMessage('Please only enter numbers')
-        .isLength({ min: 8, max: 8 })
-        .withMessage('Please enter minimun 8 characters or maximun 8 characters'),
-
-       
     
 
     
