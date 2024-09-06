623d4f75adf678cc0202b86d352e3214458bef98
clizco <gonzalezad06@gmail.com>
2024-09-05 19:22:57 -0500

    minor change in the table

--- database/db.sql
+++ database/db.sql
@@ -35,8 +35,9 @@ CREATE TABLE contacts (
 
 
 CREATE TABLE roles (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(45) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    role_name VARCHAR(45) NOT NULL,
+    created_at VARCHAR(45) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE users (
@@ -55,17 +56,28 @@ CREATE TABLE tickets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    ticket_title ENUM('Techos', 'Fachada (Estructural)', 'Estacionamientos (Estructural)', 'Losas internas', 'Goteras', 'Cimientos', 'Vidrieras', 'Limpieza Fachada', 'Cielo Raso', 'Puertas Mecanicas' 'Puertas Electricas', 'Aceras(Estructural)', 'Calles (Estructural)', 'Zonas Verdes', 'Aseo Areas Externas', 'Aire Acondicionado', 'Escaleras Electricas', 'Elevadores', 'Sistemas Contra Incendios', 'Alarma Local', 'Evaporadores', 'Compresores', 'Condensadores', 'Planta electrica', 'Transformadores', 'Material Electrico', 'Linea de Gas', 'Plomeria Enterrada en Cimientos', 'Plomeria No Enterrada en Cimientos', 'Alcantarillado', 'Tanques de Agua', 'Plantas de Tratamiento', 'Trampas de Grasa', 'Trampas de Agua' ) NOT NULL,
+    ticket_title ENUM(
+        'Techos', 'Fachada (Estructural)', 'Estacionamientos (Estructural)', 
+        'Losas internas', 'Goteras', 'Cimientos', 'Vidrieras', 'Limpieza Fachada', 
+        'Cielo Raso', 'Puertas Mecanicas', 'Puertas Electricas', 'Aceras(Estructural)', 
+        'Calles (Estructural)', 'Zonas Verdes', 'Aseo Areas Externas', 'Aire Acondicionado', 
+        'Escaleras Electricas', 'Elevadores', 'Sistemas Contra Incendios', 'Alarma Local', 
+        'Evaporadores', 'Compresores', 'Condensadores', 'Planta electrica', 'Transformadores', 
+        'Material Electrico', 'Linea de Gas', 'Plomeria Enterrada en Cimientos', 
+        'Plomeria No Enterrada en Cimientos', 'Alcantarillado', 'Tanques de Agua', 
+        'Plantas de Tratamiento', 'Trampas de Grasa', 'Trampas de Agua'
+    ) NOT NULL,
     ticket_priority ENUM('Urgente', 'Importante', 'Programable') NOT NULL DEFAULT 'Programable',
     ticket_description VARCHAR(255) NOT NULL,
     ticket_assignedto ENUM('Javier Solis', 'Ricardo Bernat') NOT NULL DEFAULT 'Javier Solis',
     file_path VARCHAR(255) DEFAULT NULL,
     video_path VARCHAR(255) DEFAULT NULL,
-    ticket_status ENUM('Open', 'in_progress', 'closed') NOT NULL DEFAULT 'open',
+    ticket_status ENUM('Open', 'in_progress', 'closed') NOT NULL DEFAULT 'Open',
     ticket_relatedto INT NOT NULL,
     user_id INT,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
 
 CREATE TABLE reports (
     id INT AUTO_INCREMENT PRIMARY KEY,
